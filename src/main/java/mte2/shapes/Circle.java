22a7e9d587208b56a40cb2f03be40bd33861e039
Daisyanet Loza-Claudio (RIT Student) <dl4966@rit.edu>
2026-03-04 10:50:53 -0500

    exercise 1 midterm

--- src/main/java/mte2/shapes/Circle.java
+++ src/main/java/mte2/shapes/Circle.java
@@ -4,14 +4,20 @@
 
 package mte2.shapes;
 
-public class Circle {
-// public class Circle implements Shape {
-    
-    // private final double radius;
-    // public Circle(double radius) {    this.radius = radius;    }
+public class Circle implements Shape {
+    private final double radius;
+    public Circle(double radius) {    
+        this.radius = radius;    
+    }
 
-    // @Override
-    // @Override
+    @Override
+    public double perimeter() {
+        return 2*Math.PI+radius;
+    }
+    @Override
+    public double area() {
+        return Math.PI+radius+radius;
+    }
 
     public static void main(String[] args) {
         
